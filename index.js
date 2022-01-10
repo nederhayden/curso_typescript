2c34b2c7adc63831d507298eb1dbe0060fc375cb
Neder <nedersimoes@gmail.com>
2022-01-10 11:55:32 -0400

    curso typescritp

---
+++ index.js
@@ -0,0 +1,9 @@
+var firstName = "Neder";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Ola " + name);
+}
+greeting(firstName);
+// greeting(anotherName)
+// greeting(x)
---
+++ index.ts
@@ -0,0 +1,11 @@
+const firstName = "Neder"
+const anotherName = 1
+const x = true
+
+function greeting(name : string) {
+  console.log("Ola " + name)
+}
+
+greeting(firstName)
+// greeting(anotherName)
+// greeting(x)
