28a919d41164615fbb29fa666a6f4c4e4df7ebc6
Neder <nedersimoes@gmail.com>
2022-01-10 14:23:06 -0400

    challenge_2

---
+++ 2_DESAFIO/index.js
@@ -0,0 +1,4 @@
+var n = 10;
+var numbertoString = n.toString();
+var printNumber = "Numero ".concat(numbertoString);
+console.log(printNumber);
---
+++ 2_DESAFIO/index.ts
@@ -0,0 +1,7 @@
+const n: number = 10
+
+const numbertoString = n.toString()
+
+const printNumber: string = `Numero ${numbertoString}`
+
+console.log(printNumber)
