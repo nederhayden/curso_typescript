079fd0bc08503aa843258c1add3e155905c9cb1b
Neder <nedersimoes@gmail.com>
2022-01-13 12:39:49 -0400

    functions

---
+++ 5_FUNCOES/index.ts
@@ -0,0 +1,128 @@
+// 1 - void
+function whitoutReturn(): void {
+  console.log("Esta funcao nao tem retorno!");
+}
+
+whitoutReturn();
+
+// 2 - callback como argumento
+function greeting(name: string): string {
+  return `Ola ${name}`;
+}
+
+function preGreeting(f: (name: string) => string, userName: string) {
+  console.log("Preparando a funcao!");
+
+  const greet = f(userName);
+
+  console.log(greet);
+}
+
+preGreeting(greeting, "Neder");
+
+// 3 - generic function
+function firstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+console.log(firstElement([1, 2, 3]));
+console.log(firstElement(["a", "b", "c"]));
+
+function mergeObjects<U, T>(obj1: U, obj2: T) {
+  return {
+    ...obj1,
+    ...obj2,
+  };
+}
+const newObject = mergeObjects(
+  { name: "Neder" },
+  { age: 30, job: "Programmer" }
+);
+
+console.log(newObject);
+
+// 4 - constraints
+function biggestNumber<T extends number | string>(a: T, b: T): T {
+  let biggest: T;
+
+  if (+a > +b) {
+    biggest = a;
+  } else {
+    biggest = b;
+  }
+
+  return biggest;
+}
+
+console.log(biggestNumber(5, 3));
+console.log(biggestNumber("3", "6"));
+
+// 5 - especificar tipo de argumento
+function mergeArray<T>(arr1: T[], arr2: T[]) {
+  return arr1.concat(arr2);
+}
+
+console.log(mergeArray([1, 2, 3], [5, 6]));
+console.log(mergeArray<number | string>([1, 2, 3], ["teste", "testando"]));
+
+// 6 - parametros opcionais
+function modernGreeting(name: string, greet?: string) {
+  if (greet) {
+    return `Ola ${greet}${name}, tudo bem?`;
+  }
+
+  return `Ola ${name}, tudo bem?`;
+}
+
+console.log(modernGreeting("Neder"));
+console.log(modernGreeting("Neder", "Dr."));
+
+// 7 - parametros default
+function somaDefault(n: number, m = 10) {
+  return n + m;
+}
+
+console.log(somaDefault(10));
+console.log(somaDefault(5, 25));
+
+// 8 - unknown
+function doSomething(x: unknown) {
+  if (Array.isArray(x)) {
+    console.log(x[0]);
+  } else if (typeof x === "number") {
+    console.log("x é um numero");
+  }
+}
+
+doSomething([1, 2, 3]);
+doSomething(1);
+
+// 9 - never
+function showErrorMessage(msg: string): never {
+  throw new Error(msg);
+}
+
+// showErrorMessage("Algum erro");
+
+// 10 - rest operator
+function sumAll(...n: number[]) {
+  return n.reduce((number, sum) => sum + number);
+}
+
+console.log(sumAll(1, 2, 3, 4, 5));
+console.log(sumAll(5, 348, 2348));
+
+// 11 - destructuring como parametro
+function showProductDetails({
+  name,
+  price,
+}: {
+  name: string;
+  price: number;
+}): string {
+  return `O nome do produto é ${name} e ele custa R$ ${price}`;
+}
+
+const shirt = { name: "Camisa", price: 49.99 };
+
+console.log(showProductDetails(shirt));
